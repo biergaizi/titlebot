cff236f3f775d63f1123180373373d75a1dcc597
Tom Li <biergaizi@member.fsf.org>
2016-07-07 08:21:08 +0800

    add magnet and i2p support
    Signed-off-by: Tom Li <biergaizi@member.fsf.org>

--- web.py
+++ web.py
@@ -1,3 +1,5 @@
+import re
+import base64
 import urllib.request
 import urllib.error
 import urllib.parse
@@ -12,7 +14,7 @@ import time
 def pickup_url(text):
     """Return a vaild URL from a string"""
 
-    PROTOCOLS = ["http:", "https:"]
+    PROTOCOLS = ["http:", "https:", "magnet:"]
     for protocol in PROTOCOLS:
         index = text.find(protocol)
         if index != -1:
@@ -27,12 +29,39 @@ def web_res_info(word):
         "size": ""
     }
 
+    def transformSpecialResource(url):
+        i2p = re.sub('http:/*([^/]+)\\.i2p(/|$)', 'http://\\1.i2p.xyz\\2', word)
+        if (i2p != word):
+            return i2p
+
+        bthash_b16 = re.findall(u'(?:\\?|&|&amp;)xt=urn:btih:([0-9A-Fa-f]{40})', word)
+        bthash_b32 = re.findall(u'(?:\\?|&|&amp;)xt=urn:btih:([2-7A-Za-z]{32})', word)
+        if bthash_b16 and bthash_b32:
+            print("Assertion Error, both bthash!")
+            print(word)
+
+        if bthash_b16:
+            querystring = bthash_b16[0].encode('utf-8', 'replace')
+        elif bthash_b32:
+            querystring = base64.b16encode(base64.b32decode(bthash_b32[0]))
+        else:
+            # no bt, do not touch url
+            return url
+
+        return 'https://btdigg.org/search?info_hash=%s' % querystring
+
+
     def openConnection(word):
         cookieJar = http.cookiejar.CookieJar()
         opener = urllib.request.build_opener(urllib.request.HTTPRedirectHandler,
                                              urllib.request.HTTPCookieProcessor(cookieJar))
         opener.addheaders = HEADERS
-        h = opener.open(word)
+
+        if "i2p.xzy" in word:
+            timeout = 60
+        else:
+            timeout = 10
+        h = opener.open(word, timeout=timeout)
 
         if h.code not in [200, 206]:
             raise urllib.error.HTTPError(code=h.code)
@@ -101,7 +130,7 @@ def web_res_info(word):
 
         return decompressed_contents
 
-
+    word = transformSpecialResource(word)
     h = openConnection(word)
 
     if h.info()["Content-Type"].split(";")[0] == "text/html" or (not "Content-Type" in h.info()):
